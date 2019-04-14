c6ccbdb920097c2ecece8f7c72568fa6c730cac7
gabrielsouzaesilva <silva.gabriels@hotmail.com>
2019-04-14 14:32:41 -0300

    DSA ok

--- T1/Intellij/src/DSA.java
+++ T1/Intellij/src/DSA.java
@@ -1,11 +1,17 @@
 import java.security.*;
 import java.security.interfaces.DSAPrivateKey;
 import java.security.interfaces.DSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class DSA {
     public static KeyPair buildKeyPair() throws NoSuchAlgorithmException {
         KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("DSA");
         keyGenerator.initialize(1024);
+
         return keyGenerator.genKeyPair();
     }
 
@@ -27,5 +33,28 @@ public class DSA {
         verifyAlgorithm.update(message);
 
         return verifyAlgorithm.verify(signature);
+    }
+
+    public static String privateKey2Str(DSAPrivateKey priv) throws GeneralSecurityException {
+        KeyFactory fact = KeyFactory.getInstance("DSA");
+        PKCS8EncodedKeySpec spec = fact.getKeySpec(priv, PKCS8EncodedKeySpec.class);
+        byte[] packed = spec.getEncoded();
+        String key64 = Base64.getEncoder().encodeToString(packed);
+
+        Arrays.fill(packed, (byte) 0);
+        return key64;
+    }
+
+    public static String publicKey2Str(DSAPublicKey pub) throws GeneralSecurityException {
+        KeyFactory fact = KeyFactory.getInstance("DSA");
+        X509EncodedKeySpec spec = fact.getKeySpec(pub, X509EncodedKeySpec.class);
+        return Base64.getEncoder().encodeToString(spec.getEncoded());
+    }
+
+    public static DSAPublicKey Str2publicKey (String pubKey_str) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] publicBytes = Base64.getDecoder().decode(pubKey_str);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("DSA");
+        return (DSAPublicKey) keyFactory.generatePublic(keySpec);
     }
 }
--- T1/Intellij/src/Main.java
+++ T1/Intellij/src/Main.java
@@ -6,28 +6,41 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String multicast_ip = "224.0.0.42";
-        int porta = 6789;
+        //String multicast_ip = "224.0.0.42";
+        //int porta = 6789;
 
-        MulticastPeer processo1 = new MulticastPeer(multicast_ip,porta,1);
-        MulticastPeer processo2 = new MulticastPeer(multicast_ip,porta,2);
+        //MulticastPeer processo1 = new MulticastPeer(multicast_ip,porta,1);
+        //MulticastPeer processo2 = new MulticastPeer(multicast_ip,porta,2);
         //MulticastPeer processo3 = new MulticastPeer(multicast_ip,porta,3);
 
-        System.out.println("[main] Processos criados!!");
+        //System.out.println("[main] Processos criados!!");
+
+        DSA cripto = new DSA();
+        String msg = "Oi teste";
+
+        KeyPair keys = cripto.buildKeyPair();
+
+        privKey = keys.getPrivate();
+        pubKey = keys.getPublic();
+
+        pubKey_str = cripto.publicKey2Str(pubKey);
 
         try {
             //deletar o processo 1 depois de 10 segundos
             //Thread.sleep(10000);
             //processo1.desativa();
             //System.out.println("[main] Processo 1 deletado");
+            System.out.println("pubKey: ", pubKey_str);
 
-            int segundos=0;
-            //infinite loop
-            while(true){
-                System.out.printf("[main] %3d segundos\n", segundos);
-                Thread.sleep(1000);
-                segundos+=1;
-            }
+            //int segundos=0;
+            ////infinite loop
+            //while(true){
+            //    System.out.printf("[main] %3d segundos\n", segundos);
+            //    Thread.sleep(1000);
+            //    segundos+=1;
+            //}
+
+
         }catch (InterruptedException e){System.out.println("Interrupt: " + e.getMessage());}
 
 
