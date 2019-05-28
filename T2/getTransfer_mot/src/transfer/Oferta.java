80d1175ccbba8f1be87866587e0277f59b2bbe65
cordeirolibel <gustavolibel2@gmail.com>
2019-05-27 21:42:58 -0300

    add oferta

--- T2/getTransfer_mot/src/transfer/Oferta.java
+++ T2/getTransfer_mot/src/transfer/Oferta.java
@@ -3,8 +3,8 @@ package transfer;
 import java.io.Serializable;
 import java.util.Date;
 
-public class Oferta implements Serializable {
-    String itinerario;
+public class Oferta  implements Serializable {
+    //String itinerario;
     String veiculo;
     int passageiros;
     float preco;
@@ -14,7 +14,9 @@ public class Oferta implements Serializable {
     }
 
     public void print(){
-
+        System.out.printf("Veiculo     %s\n",veiculo);
+        System.out.printf("Preco       %.2f\n",preco);
+        System.out.printf("Passageiros %d\n",passageiros);
     }
 
 }
